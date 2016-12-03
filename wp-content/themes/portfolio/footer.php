f10663b4caa20844b5ed6a4c07fbf7037d8db3e2
Alex Sandro de Almeida <sanderghost@gmail.com>
2016-12-03 14:47:09 -0200

    Atualizado
    index

---
+++ wp-content/themes/portfolio/footer.php
@@ -0,0 +1,8 @@
+<?php wp_footer(); ?>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="\assets\bootstrap-3.3.7-dist\js/bootstrap.min.js"></script>
+        <script src="\assets\bootstrap-3.3.7-dist\js/bootstrap.js"></script>
+</body>
+</html>
--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,61 +1,9 @@
-
-    
-      
-      
-       <body>
-        <h1>Hello, world!</h1>
+<?php get_header(); ?>       
         
-        
-        
-        <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<ul class="nav nav-pills">
-				<li class="active">
-					<a href="#">Home</a>
-				</li>
-				<li>
-					<a href="#">Profile</a>
-				</li>
-				<li class="disabled">
-					<a href="#">Messages</a>
-				</li>
-				<li class="dropdown pull-right">
-					 <a href="#" data-toggle="dropdown" class="dropdown-toggle">Dropdown<strong class="caret"></strong></a>
-					<ul class="dropdown-menu">
-						<li>
-							<a href="#">Action</a>
-						</li>
-						<li>
-							<a href="#">Another action</a>
-						</li>
-						<li>
-							<a href="#">Something else here</a>
-						</li>
-						<li class="divider">
-						</li>
-						<li>
-							<a href="#">Separated link</a>
-						</li>
-					</ul>
-				</li>
-			</ul><img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-			<p>
-				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
-			</p> 
-			<button type="button" class="btn btn-default">
-				Default
-			</button>
+    <div class="container-fluid">
+	    <div class="row">
+		    <div class="col-md-12">
+                
 		</div>
 	</div>
 </div>
-        
-        
-
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="\assets\bootstrap-3.3.7-dist\js/bootstrap.min.js"></script>
-        <script src="\assets\bootstrap-3.3.7-dist\js/bootstrap.js"></script>
-    </body>
-</html>
